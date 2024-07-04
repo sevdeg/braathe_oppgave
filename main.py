8f6addda8b40df673eed9e1fa445d1168e3e152d
sevdeg <sevde25_2@hotmail.com>
2024-07-04 19:02:11 +0200

    Endret litt på redis og lagt til & C:/Users/sevde/AppData/Local/Programs/Python/Python312/python.exe c:/Users/sevde/Documents/Braathe_oppgave/main.pyAlgolia søk

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from tabulate import tabulate
 import redis
 import json
+from algoliasearch.search_client import SearchClient
 
 #Legger til data
 my_data = [
-    [1, 'Ella Pia', 1996],
-    [2, 'Petter Havn', 2000],
-    [3, 'Sevde oguz', 2000]
+    {"ID": 1, "Navn Etternavn": 'Ella Pia',"År": 1996},
+    {"ID": 2, "Navn Etternavn": 'Petter Havn',"År": 2000},
+    {"ID":3,"Navn Etternavn": 'Sevde Oguz',"År": 2000}
 ]
 
-#Legger til overskrift på tabellen
+#Konverterer til en liste av lister
+list = [[employee["ID"], employee["Navn Etternavn"], employee["År"]] for employee in my_data]
+
+#Legger til overskrifter på tabellen
 headers = ['ID', 'Navn Etternavn', 'År']
+
+#Printer ut tabellen
+print("\nTabell med tabulate:")
+print(tabulate(list, headers=headers, tablefmt="pretty"))
 
 #Kobler til Redis serveren
 r = redis.Redis(host='localhost', port=6379)
@@ -22,15 +30,27 @@ r.delete('employees')
 for employee in my_data:
     r.rpush('employees', json.dumps(employee))
 
-#Henter ut dataene fra Redis listen
-r_data = r.lrange('employees', 0, -1)
-data_redis = [json.loads(data) for data in r_data]
-
 #Skriver ut data fra redis
-print("Data fra Redis liste:")
-for idx, employee in enumerate(data_redis):
-    print(f"employee {idx+1}: {employee}")
+print("\nData fra Redis liste:")
+for index, data in enumerate(r.lrange('employees',0,-1)):
+    employee_dict = json.loads(data)
+    print(f"employee {index+1}: {employee_dict}")
 
-#Printer ut tabellen
-print("\nTabell med tabulate:")
-print(tabulate(my_data, headers=headers, tablefmt="pretty"))
+#Algolia tilkobling
+client = SearchClient.create('RRKOZQ1G6S', '1e8d734876b3e43dac50c6e2bf47e1bf')
+index = client.init_index('employees')
+obj_id = [{"objectID": str(index+1), **employee} for index, employee in enumerate(my_data)]
+index.save_objects(obj_id)
+
+#Søk år
+search_year = 1996
+results = index.search('', {
+    'filters': f'År = {search_year}'
+})
+
+#Skrive ut søke med tabulate
+search = results['hits']
+res_table = [[employee['ID'], employee['Navn Etternavn'], employee['År']] for employee in search]
+
+print(f"\nResultat av søking ved år {search_year}: ")
+print(tabulate(res_table, headers=headers, tablefmt="pretty"))
